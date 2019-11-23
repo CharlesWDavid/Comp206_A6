6d8768b0283bccdb2e24a2594c926fc4f3917704
Charles David <charleswd@hotmail.ca>
2019-11-23 16:00:21 -0500

    added a condition that skips blank lines in ssv files

--- main.c
+++ main.c
@@ -11,14 +11,18 @@ int main(){
     char line[30];
     for (int i=0; i<30; i++) line[i] = '\0'; // wiping array at each read
     
-    FILE *p=fopen("students.ssv", "rt");
+    FILE *p=fopen("mini6tester.ssv", "rt");
     if(p == NULL) return 1;
     
-    while(fgets(line, 29, p) != NULL){
-        int *acct;
-        float *amnt; // creating pointers to pass them into multiple functions
-        parse(line, acct, amnt);
-        findUpdate(*acct, *amnt);
+    while(fgets(line, 29, p) != NULL){ 
+        if (line[0] != '\n'){ // skip blank lines
+            int *acct;
+            float *amnt; // creating pointers to pass them into multiple functions
+            parse(line, acct, amnt);
+            findUpdate(*acct, *amnt);
+        } else {
+            continue;
+        } 
     }
     fclose(p);
     prettyPrint();
